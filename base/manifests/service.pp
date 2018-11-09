35f0187eef7d6a645a7e1def082924c10d508b4a
shengnanzhang <35984994+shengnanzhang@users.noreply.github.com>
2018-11-09 11:02:47 +0800

    Update service.pp

--- base/manifests/service.pp
+++ base/manifests/service.pp
@@ -1,15 +1,16 @@
 #策略用途：启停服务
 #策略说明：指定的服务确保运行状态以及设置为开机自启动
-#策略风险：
-#策略验证：
-#使用说明：
+#策略风险：无
+#策略验证：查看服务是否启动 service sshd status
+#使用说明：参数ensure确保服务运行，enable服务开机是否自启动，hasrestart是否init脚本可以重启此服务，hasstatus是否init脚本
+#         查看此服务状态
 #case记录：
 
 class base::service {
 	service { ["sshd", "ntpd", "crond"]:
 		ensure => "running",
 		enable => "true",
-		hasrestart => true, #是否支持restart参数,不支持就用stop和start实现重启
-		hasstatus => true,  #是否支持status参数,不支持的话查找进程列表来判断服务状态
+		hasrestart => true, 
+		hasstatus => true,  
         }
 }
